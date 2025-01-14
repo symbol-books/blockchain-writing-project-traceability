4c79561c8d93ab21155128b6cef14808dcbd8ab2
hossiiii <hossiiii0117@gmail.com>
2025-01-15 08:45:15 +0900

    ノードリストの修正

--- src/consts/nodeList.ts
+++ src/consts/nodeList.ts
@@ -1,6 +1,6 @@
 export const nodeList = [
   'https://sym-test-01.opening-line.jp:3001',
   'https://sym-test-03.opening-line.jp:3001',
-  'https://test01.xymnodes.com:3001',
-  'https://test02.xymnodes.com:3001',
+  // 'https://test01.xymnodes.com:3001',  2025/01/15 ノードリストから除外
+  // 'https://test02.xymnodes.com:3001',  2025/01/15 ノードリストから除外
 ];
